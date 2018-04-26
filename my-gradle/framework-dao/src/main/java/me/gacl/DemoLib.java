d18ea72454a52dfdbbedcbb6c01d3b384faece2b
jingquanliang <540692237@qq.com>
2018-04-26 17:24:50 +0800

    gradle 多项目模板文件

---
+++ my-gradle/framework-dao/src/main/java/me/gacl/DemoLib.java
@@ -0,0 +1,9 @@
+package me.gacl;
+
+
+public class DemoLib {
+
+    public static void demo() {
+        System.out.println("我是DemoLib中的demo方法");
+    }
+}
---
+++ my-gradle/framework-pcweb/src/main/java/hello/HelloController.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/index")
+    public String index() {
+        System.out.println("======================================");
+        return "index";
+    }
+
+}
